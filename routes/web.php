6b58222addbaa9a926b42d5bd767ceb9cc770871
Alex JL <alexsrk09@gmail.com>
2025-01-09 20:31:55 +0100

    welcome 2

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'wellcomeWithData'])->name('welcome');
 Route::get('/sesion',function(){return view('welcome1');})->name('sesion');
+Route::get('/sesion1',function(){return view('welcome2');})->name('sesion');
+
 Route::get('/map',  [MapController::class,'index']);
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 Route::post('/map/create',  [MapController::class,'create'])->name('create');
