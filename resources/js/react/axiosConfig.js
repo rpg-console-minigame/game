7bd9fcce2bd756e42f3892affe35a5b4f87d63bb
aqp0001 <aqp0001@alu.medac.es>
2025-05-05 00:48:48 +0200

    a

--- resources/js/react/axiosConfig.js
+++ resources/js/react/axiosConfig.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 // Set the base URL for all axios requests
-axios.defaults.baseURL = import.meta.env.API_URL; // Default to localhost if VITE_API_URL is not set
-// axios.defaults.baseURL = 'http://10.192.81.1:8000';
+// axios.defaults.baseURL = import.meta.env.API_URL; // Default to localhost if VITE_API_URL is not set
+axios.defaults.baseURL = 'https://game-production-0fc8.up.railway.app/api';
 // axios.defaults.baseURL = 'http://192.168.8.116:8000';
 
 
