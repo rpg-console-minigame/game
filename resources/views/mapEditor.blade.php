5bc1958985e22a304cb9f2235f2a68890a769150
aqp0001 <153547954+aqp0001@users.noreply.github.com>
2025-05-05 10:41:31 +0200

    d

--- resources/views/mapEditor.blade.php
+++ resources/views/mapEditor.blade.php
@@ -138,16 +138,8 @@
                         echo 'border-right: 2px solid black;';
                     } ?>">
                         {{-- <a href="{{ route('delete', ['coord_x' => $i, 'coord_y' => $j]) }}">Edit</a> --}}
-<!--                         <p type="button" data-toggle="modal"
-                            data-target="#modalEdit{{ $i }}-{{ $j }}">Edit</p> -->
-
-                        
-                        <div class="col d-flex align-items-center justify-content-center" 
-                         style="height: 50px; width: 50px; background-color: #FF0000; border-radius: 8px; cursor: pointer; transition: background-color 0.3s; margin: 4px;"
-                         data-toggle="modal" data-target="#modalEdit{{ $i }}-{{ $j }}">
-                        <p class="mb-0 text-white text-center" style="font-size: 12px;">Create</p>
-
-                            
+                        <p type="button" data-toggle="modal"
+                            data-target="#modalEdit{{ $i }}-{{ $j }}">Edit</p>
                         <div class="modal fade" id="modalEdit{{ $i }}-{{ $j }}" tabindex="-1"
                             role="dialog">
                             <div class="modal-dialog modal-dialog-centered" role="document">
