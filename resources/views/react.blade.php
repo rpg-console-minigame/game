5860e6cc81b012e78c6285d877001b58ede17840
aqp0001 <153547954+aqp0001@users.noreply.github.com>
2025-05-05 09:25:38 +0200

    Update react.blade.php

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vite + React</title>
-    @Vite(['resources/js/react/main.jsx'])
+    @vite(['resources/js/react/main.jsx'])
   </head>
   <body  style= "background-color: #647994">
     <div id="root"></div>
