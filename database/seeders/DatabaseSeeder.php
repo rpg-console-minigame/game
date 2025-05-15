0ec443227d24ad577c6595777abc9b3ebc1be64e
aqp0001 <153547954+aqp0001@users.noreply.github.com>
2025-05-15 12:45:22 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,37 +10,37 @@ use App\Models\TiendaOro;
 
 class DatabaseSeeder extends Seeder
 {
-    // /**
-    //  * Seed the application's database. 
-    //  *
-    //  * @return void
-    //  */
-    // public function run()
-    // {
-    //     // Crear zonas
-    //     $contador = 0;
-    //     for ($x = 0; $x <= 2; $x++) {
-    //         for ($y = 0; $y <= 2; $y++) {
-    //             $contador++;
-    //             Zona::factory()->create([
-    //                 'nombre' => 'Sala ' . $contador,
-    //                 'descripcion' => 'Sala ' . $contador,
-    //                 'imagen' => 'a',
-    //                 'down_door' => true,
-    //                 'left_door' => true,
-    //                 'right_door' => true,
-    //                 'up_door' => true,
-    //                 'coord_x' => $x,
-    //                 'coord_y' => $y,
-    //             ]);
-    //         }
-    //     }
+    /**
+     * Seed the application's database. 
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Crear zonas
+        $contador = 0;
+        for ($x = 0; $x <= 2; $x++) {
+            for ($y = 0; $y <= 2; $y++) {
+                $contador++;
+                Zona::factory()->create([
+                    'nombre' => 'Sala ' . $contador,
+                    'descripcion' => 'Sala ' . $contador,
+                    'imagen' => 'a',
+                    'down_door' => true,
+                    'left_door' => true,
+                    'right_door' => true,
+                    'up_door' => true,
+                    'coord_x' => $x,
+                    'coord_y' => $y,
+                ]);
+            }
+        }
 
-    //     // Crear adminUser con usuario y contraseña "admin"
-    //     adminUser::create([
-    //         'name' => 'admin',
-    //         'password' => Hash::make('admin'),
-    //     ]);
+        // Crear adminUser con usuario y contraseña "admin"
+        adminUser::create([
+            'name' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
 
         // Insertar paquetes de oro
          TiendaOro::insert([
