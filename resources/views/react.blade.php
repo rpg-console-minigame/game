dae1aec3dadb284e888dab3d94ea6713c150b514
aqp0001 <153547954+aqp0001@users.noreply.github.com>
2025-05-20 09:30:37 +0200

    titulo react.blade

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite + React</title>
+    <title>Juega y Conquista</title>
     @vite(['resources/js/react/main.jsx'])
   </head>
   <body  style= "background-color: #647994">
